563bf37016afb1416f90b2d076d05f40e635671f
Juan Manuel Lopez <jlopezjuy@gmail.com>
2021-01-29 11:39:39 -0300

    Se elimina configuraciones de eureka.

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -12,43 +12,15 @@ const commonConf = {
 
 let ormconfig: TypeOrmModuleOptions = {
   name: 'default',
-  type: 'sqlite',
-  database: '../target/sqlite-dev-db.sql',
-  logging: true,
-  synchronize: true,
+  type: 'mysql',
+  database: 'OCMSoliServer',
+  url: process.env.DATABASE_URL,
+  logging: false,
+  synchronize: commonConf.SYNCRONIZE,
   entities: commonConf.ENTITIES,
   migrations: commonConf.MIGRATIONS,
   cli: commonConf.CLI,
   migrationsRun: commonConf.MIGRATIONS_RUN
 };
-
-if (process.env.NODE_ENV === 'prod') {
-  ormconfig = {
-    name: 'default',
-    type: 'mysql',
-    database: 'OCMSoliServer',
-    url: process.env.DATABASE_URL,
-    logging: false,
-    synchronize: commonConf.SYNCRONIZE,
-    entities: commonConf.ENTITIES,
-    migrations: commonConf.MIGRATIONS,
-    cli: commonConf.CLI,
-    migrationsRun: commonConf.MIGRATIONS_RUN
-  };
-}
-
-if (process.env.NODE_ENV === 'test') {
-  ormconfig = {
-    name: 'default',
-    type: 'sqlite',
-    database: ':memory:',
-    logging: true,
-    synchronize: true,
-    entities: commonConf.ENTITIES,
-    migrations: commonConf.MIGRATIONS,
-    cli: commonConf.CLI,
-    migrationsRun: commonConf.MIGRATIONS_RUN
-  };
-}
 
 export { ormconfig };
