1c5ae6e0302ac5f023c03798c9deaa83f638fb7f
Miguel Angel Martinez <correomam@gmail.com>
2021-02-26 09:55:23 +0100

    Cambios menores.

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -15,28 +15,22 @@ export class AuthService {
     private readonly jwtService: JwtService,
     @InjectRepository(AuthorityRepository) private authorityRepository: AuthorityRepository,
     private userService: UserService
-  ) {}
+  ) { }
 
   async login(userLogin: UserLoginDTO): Promise<any> {
     const loginUserName = userLogin.username;
     const loginPassword = userLogin.password;
-
     const userFind = await this.userService.findByfields({ where: { login: loginUserName, password: loginPassword } });
     if (!userFind) {
       throw new HttpException('Invalid login name or password.', HttpStatus.BAD_REQUEST);
     }
-
     const user = await this.findUserWithAuthById(userFind.id);
-
     const payload: Payload = { id: user.id, username: user.login, authorities: user.authorities };
-
-    /* eslint-disable */
     return {
       id_token: this.jwtService.sign(payload)
     };
   }
 
-  /* eslint-enable */
   async validateUser(payload: Payload): Promise<User | undefined> {
     return await this.findUserWithAuthById(payload.id);
   }
@@ -49,4 +43,5 @@ export class AuthService {
     const user: any = await this.userService.findByfields({ where: { id: userId } });
     return user;
   }
+
 }
