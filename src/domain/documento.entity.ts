787116f56ae1e0d20b566616577e4b3330c47c2d
Miguel Angel Martinez <correomam@gmail.com>
2021-04-20 11:22:51 +0200

    Mejoras en solicitud

--- src/domain/documento.entity.ts
+++ src/domain/documento.entity.ts
@@ -1,37 +1,29 @@
 import { Entity, Column, ManyToOne } from 'typeorm';
 import { BaseEntity } from './base/base.entity';
-
 import Solicitud from './solicitud.entity';
-import Gestion from './gestion.entity';
+
 @Entity('documento')
 export default class Documento extends BaseEntity {
-  @Column({ name: 'nombre_de_documento' })
-  nombreDeDocumento: string;
+  @Column({ name: 'nombreDocumento' })
+  nombreDocumento: string;
 
-  @Column({ type: 'date', name: 'fecha_subida' })
+  @Column({ type: 'date' })
   fechaSubida: any;
 
-  @Column({ type: 'blob', name: 'documento', nullable: true })
-  documento: any;
-
-  @Column({ name: 'documento_content_type', nullable: true })
-  documentoContentType: string;
+  @Column({ nullable: true })
+  documentoType: string;
   
-  @Column({ name: 'observacion', nullable: true })
+  @Column({ nullable: true })
   observacion: string;
 
-  @Column({ name: 'ruta', nullable: true })
+  @Column({ nullable: true })
   ruta: string;
 
-  @Column({ type: 'boolean', name: 'privado', nullable: true })
+  @Column({ type: 'boolean', nullable: true })
   privado: boolean;
 
   @ManyToOne(type => Solicitud)
   solicitud: Solicitud;
-
   solicitudId?: string;
-
-  @ManyToOne(type => Gestion)
-  gestion: Gestion;
 
 }
